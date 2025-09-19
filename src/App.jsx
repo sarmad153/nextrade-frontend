e8a24fa1539c20d97173d3ae1f1219bef9586502
prodeveloperjunaid <hello.junaid2311@gmail.com>
2025-09-20 00:26:36 +0500

    Updated App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Register from './pages/Auth/Register'
 import CategoryManagement from './pages/Admin/CategoryManagement'
 import Login from './pages/Auth/Login'
 import AddProduct from './pages/Seller/AddProduct'
+import ManageProducts from './pages/Seller/ManageProducts '
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path='/login' element={<Login />} />
       <Route path='/category' element={<CategoryManagement />} />
       <Route path='/addproduct' element={<AddProduct />} />
+      <Route path='/manageproducts' element={<ManageProducts />} />
     </Routes>
   )
 }
